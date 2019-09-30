fa3e7027f749795df57b525c2a39eb99341a787b
prosperos <blazhivsky12@gmail.com>
2019-09-30 15:28:20 +0300

    first commit

---
+++ src/components/Home/HomeBanner/HomeBanner.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react'
+
+export default class HomeBanner  extends Component{
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+
+                            <p>HEllo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
